67b0a7d3698f4c1c6849b75988e9c216be0c88cc
Mohammad Ahmed <yahmed.mohammad@gmail.com>
2022-08-19 11:37:48 +0530

    Create 404.js

---
+++ src/components/404.js
@@ -0,0 +1,14 @@
+import {connect} from "react-redux";
+
+const Error404 = () => {
+    return (
+        <div>
+            <h1>Error 404</h1>
+            <h2>Page not found</h2>
+        </div>
+    );
+};
+
+const mapStateToProps = () => ({});
+
+export default connect(mapStateToProps)(Error404);
